a92e3417e7b3a039b9c6572ce075b3d94e24454e
SahdevPrajapati18 <sahdevprajapati27@gmail.com>
2025-10-27 01:44:43 +0530

    .

--- auth/src/app.js
+++ auth/src/app.js
@@ -10,28 +10,29 @@ import cors from 'cors';
 
 dotenv.config();
 
-
 const app = express();
-app.use(cors({
-    origin: 'https://madmax-nine.vercel.app',
-    credentials: true,
-}))
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ['https://madmax-nine.vercel.app'], // your frontend URL
+    credentials: true, // allow cookies cross-origin
+  })
+);
 
 app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,    
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: '/api/auth/google/callback'
 },(accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }));
 
-
-
 app.use(morgan('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
--- music/src/app.js
+++ music/src/app.js
@@ -5,13 +5,16 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(
+  cors({
     origin: ['https://madmax-nine.vercel.app'],
     credentials: true
-}));
+  })
+);
 
-app.use(express.json());
-app.use(cookieParser());
 app.use('/api/music', musicRoutes);
 
 export default app;
